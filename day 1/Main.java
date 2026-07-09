6ecc3f8cf59ef4cc6e220024e788d5c881e5013d
venkatesh006-hub <itsmevenkat006@gmail.com>
2026-07-09 20:27:53 +0530

    Add files via upload

---
+++ day 1/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int a = sc.nextInt();
+        System.out.print("Enter a number:");
+        int b = sc.nextInt();
+        System.out.println(a+b+" "+"is the answer");
+    }
+}
